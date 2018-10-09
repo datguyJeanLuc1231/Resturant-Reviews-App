6c922b160ebecb263bc81c0b0a445beca6b7d886
Jean-Luc-Adams <jladams91@gmail.com>
2018-10-09 12:30:53 -0500

    Added the MapBox API to the restaurant info js file as well

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiamxhZGFtczkxIiwiYSI6ImNqbjF6dmt5YjAwZjIzd3BhZ2hscXRsd2kifQ.imnyWvBUi6F0d6CQaim63Q',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
